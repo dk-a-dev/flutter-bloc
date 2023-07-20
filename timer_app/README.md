af30279b06252b6d9cc09fa6cda47703ec6badc8
Dev Keshwani <dev.keshwani345@gmail.com>
2023-07-20 23:48:55 +0530

    feat: timer states created

--- timer_app/README.md
+++ timer_app/README.md
@@ -1,2 +1,4 @@
 # timer_app
 A simple Timer app made using bloc
+
+- > **Ticker** is a class that emits a stream of integers at a specified interval.
--- timer_app/lib/timer/bloc/timer_event.dart
+++ timer_app/lib/timer/bloc/timer_event.dart
@@ -1,4 +1,27 @@
 part of 'timer_bloc.dart';
 
-@immutable
-abstract class TimerEvent {}
+sealed class TimerEvent {
+  const TimerEvent();
+}
+
+final class TimerStarted extends TimerEvent {
+  final int duration;
+  const TimerStarted({required this.duration});
+}
+
+final class TimerPaused extends TimerEvent {
+  const TimerPaused();
+}
+
+final class TimerResumed extends TimerEvent {
+  const TimerResumed();
+}
+
+class TimerReset extends TimerEvent {
+  const TimerReset();
+}
+
+class _TimerTicked extends TimerEvent {
+  final int duration;
+  const _TimerTicked({required this.duration});
+}
