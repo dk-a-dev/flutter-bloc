566c9c86a805ccd4c02bbd02cf1153e5b555e5d9
Dev Keshwani <dev.keshwani345@gmail.com>
2023-07-21 11:20:03 +0530

    feat:app ui added

---
+++ timer_app/lib/timer/view/timer_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:timer_app/timer/view/background.dart';
+import 'package:timer_app/timer/view/timer_text.dart';
+import 'package:timer_app/timer/view/action_buttons.dart';
+
+
+class TimerView extends StatelessWidget {
+  const TimerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Timer')),
+      body: const Stack(
+        children: [
+          Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(child: TimerText()),
+              ),
+              ActionButtons(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
